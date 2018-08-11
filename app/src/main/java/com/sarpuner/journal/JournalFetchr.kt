2b36179ab9a1e14e63921b3c987dbe8408869be9
Sarp Uner <su26@duke.edu>
2018-08-11 01:18:48 -0400

    Added the jaudiotagger library to gradle.

--- app/src/main/java/com/sarpuner/journal/JournalFetchr.kt
+++ app/src/main/java/com/sarpuner/journal/JournalFetchr.kt
@@ -4,8 +4,8 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
-import org.json.JSONObject
 import org.jsoup.Jsoup
+
 
 /* The methods in this file will be called in an asynctask. Service may be an overkill, because we
 do not wish it to run continuously. */
