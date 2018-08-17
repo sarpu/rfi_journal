b9b34ad84a378ac75d016b1667393e42e36ea6fe
Sarp Uner <su26@duke.edu>
2018-08-17 16:15:05 -0400

    App crashes upon clicking download button. Trying to debug.

--- app/src/main/java/com/sarpuner/journal/DownloadIntentService.kt
+++ app/src/main/java/com/sarpuner/journal/DownloadIntentService.kt
@@ -4,13 +4,9 @@ import android.app.IntentService
 import android.content.Intent
 
 class DownloadIntentService : IntentService(DownloadIntentService::class.simpleName) {
-    val epiList: List<Episode> = parseMainPage()
 
 
     override fun onHandleIntent(downloadIntent: Intent) {
         val epiList: List<Episode> = parseMainPage()
-
-
-
     }
 }
--- app/src/main/java/com/sarpuner/journal/MainActivity.kt
+++ app/src/main/java/com/sarpuner/journal/MainActivity.kt
@@ -11,10 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        parseMainPage()
-        val contxt = this.filesDir
-
     }
 
     // TODO: Add a scrolling list of RFI episodes.
